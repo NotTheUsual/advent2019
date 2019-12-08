62ced6c4035cc376e29352e201004e51752a3a6a
James Hunter <james.hunter@starlingbank.com>
2019-12-08 21:44:49 +0000

    New day shell script

---
+++ new-day.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$2" = "b" ]
+then
+  touch day$1/day$1b.js
+  touch day$1/day$1b.spec.js
+else
+  mkdir day$1
+  touch day$1/day$1a.js
+  touch day$1/day$1a.spec.js
+  touch input.txt
+fi
